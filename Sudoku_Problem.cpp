2f7accf842914c01dd92ac28c9bab9ebb1578cd0
Nitesh Singh <103584809+NiteshSingh1212@users.noreply.github.com>
2023-03-27 16:13:30 +0530

    Add files via upload

---
+++ Sudoku_Problem.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+     bool isSafe(vector<vector<char>>& board,int row,int col,char val){
+         for(int i=0;i<board.size();i++){
+          if(board[i][col]==val){
+              return false;
+          }
+          if(board[row][i]==val){
+              return false;
+          }
+          if(board[3*(row/3)+i/3][3*(col/3)+i%3]==val){
+              return false;
+          }
+         }return true;
+     }
+    bool solve(vector<vector<char>>& board){
+      
+       for(int i=0;i<board.size();i++){
+           for(int j=0;j<board.size();j++){
+              if( board[i][j]=='.'){
+                   for(char val='1';val<='9';val++){
+                        if (isSafe(board,i,j,val)){
+                       board[i][j]=val;
+                      if(solve(board)==true){
+                          return true;
+                      }
+                      else{
+                          board[i][j]='.';
+                      }
+                   }
+                 }
+                   return false;
+              }
+           }
+       }
+       return true;
+    }
+    void solveSudoku(vector<vector<char>>& board){ 
+    solve(board);  
+    }
+};
